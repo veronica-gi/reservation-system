7f9934b4ff4d8be684d5ce52bd0857c041282fbe
Verónica García <veronicagaiglesias@gmail.com>
2026-03-13 17:19:51 +0100

    feat: Add Service REST controller

---
+++ backend/src/main/java/com/veronica/reservation_system/controller/ServiceController.java
@@ -0,0 +1,26 @@
+package com.veronica.reservation_system.controller;
+
+import com.veronica.reservation_system.model.Service;
+import com.veronica.reservation_system.repository.ServiceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/services")
+public class ServiceController {
+
+    @Autowired
+    private ServiceRepository serviceRepository;
+
+    @GetMapping
+    public List<Service> getAllServices() {
+        return serviceRepository.findAll();
+    }
+
+    @PostMapping
+    public Service createService(@RequestBody Service service) {
+        return serviceRepository.save(service);
+    }
+}
