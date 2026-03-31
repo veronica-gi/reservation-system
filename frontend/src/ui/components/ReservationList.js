0dabe7a60a339469dead7da68f7703d306d18672
Verónica García <veronicagaiglesias@gmail.com>
2026-03-31 19:15:52 +0200

    style: improve the style and format of reservations with arrows, colors and a calendar

--- frontend/src/ui/components/ReservationList.js
+++ frontend/src/ui/components/ReservationList.js
@@ -12,8 +12,12 @@ function ReservationList({ reservations, onDelete, onEdit }) {
        .sort((a, b) => new Date(a.date) - new Date(b.date))
         .map(r => (
         <li key={r.id}>
+
   <span className="reservation-info">
-    {r.clientName} - {r.service?.name} - {new Date(r.date).toLocaleString()}
+   <span className="reservation-client">{r.clientName}</span>
+          <span className="arrow">→</span>
+    <span className="reservation-service">{r.service?.name}</span>
+    <span className="reservation-date">📅 {new Date(r.date).toLocaleString()}</span>
   </span>
 
   <div className="actions">
